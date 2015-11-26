2167420d7d4bfdaa8483b29808fcd92c2c3464c5
Jacob Prud'homme <jacob.pkmn@gmail.com>
2015-11-26 13:54:54 -0500

    Maybe?

--- HallwayLights/HallwayLights.ino
+++ HallwayLights/HallwayLights.ino
@@ -1,3 +1,7 @@
+#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message
+
 #include <Time.h>
 #include <TimeLib.h>
 
@@ -13,6 +17,8 @@ void setup() {
 }
 
 void loop() {
+  processSyncMessage();
+  
   switch(weekday()) {
     case 1:
       Serial.println("Saturday");
@@ -55,4 +61,22 @@ void loop() {
   }
 
   delay(3600000); // Check every hour
+}
+
+void processSyncMessage() {
+  // if time sync available from serial port, update time and return true
+  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of header & 10 ASCII digits
+    char c = Serial.read() ;
+    Serial.print(c);  
+    if( c == TIME_HEADER ) {      
+      time_t pctime = 0;
+      for(int i=0; i < TIME_MSG_LEN -1; i++){  
+        c = Serial.read();          
+        if( c >= '0' && c <= '9'){  
+          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
+        }
+      }  
+      setTime(pctime);   // Sync Arduino clock to the time received on the serial port
+    }  
+  }
 }
