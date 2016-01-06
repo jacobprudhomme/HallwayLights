1c11a968569a0d25e96e6d63a81f31a3411eb30c
Jacob Prud'homme <jacob.pkmn@gmail.com>
2016-01-06 15:48:15 -0500

    Comments galore

--- HallwayLights/HallwayLights.ino
+++ HallwayLights/HallwayLights.ino
@@ -1,91 +1,91 @@
-#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
-#define TIME_HEADER  'T'   // Header tag for serial time sync message
-#define TIME_REQUEST  7    // ASCII bell character requests a time sync message
+#define TIME_MSG_LEN  11   // Longueur du message à être reçu du sketch Processing
+#define TIME_HEADER  'T'   // En-tête qui déclare que le message reçu du sketch Processing est pour synchroniser le temps
+#define TIME_REQUEST  7    // Requête envoyée au sketch Processing pour demander une synchroniser du temps
 
-#include <Time.h>     // Time library
-#include <TimeLib.h>  // Time library
+#include <Time.h>     // Module pour les fonctions de temps
+#include <TimeLib.h>  // Module pour les fonctions de temps
 
-int hours_red_reg[] = {9, 10, 12, 14};            // Les heures de commencement des périodes
-int minutes_red_reg[] = {15, 40, 55, 15};         // Les minutes de commencement des périodes
-int hours_green_reg[] = {10, 11, 14, 15};         // Les heures de fin des périodes
-int minutes_green_reg[] = {30, 55, 5, 30};        // Les minutes de fin des périodes
-int hours_yellow_reg[] = {9, 10, 12, 14};         // Les heures d'alerte au commencement des périodes
-int minutes_yellow_reg[] = {12, 37, 52, 12};      // Les minutes d'alerte au commencement des périodes
+int hours_red_reg[] = {9, 10, 12, 14};            // Les heures de commencement des périodes sur une journée normale
+int minutes_red_reg[] = {15, 40, 50, 15};         // Les minutes de commencement des périodes sur une journée normale
+int hours_green_reg[] = {10, 11, 14, 15};         // Les heures de fin des périodes sur une journée normale
+int minutes_green_reg[] = {30, 55, 5, 30};        // Les minutes de fin des périodes sur une journée normale
+int hours_yellow_reg[] = {9, 10, 12, 14};         // Les heures d'alerte au commencement des périodes sur une journée normale
+int minutes_yellow_reg[] = {12, 37, 47, 12};      // Les minutes d'alerte au commencement des périodes sur une journée normale (3 minutes avant la cloche)
 int hours_red_cap[] = {9, 10, 11, 13, 14};        // Les heures de commencement des périodes sur une journée CAP
 int minutes_red_cap[] = {15, 22, 30, 22, 30};     // Les minutes de commencement des périodes sur une journée CAP
 int hours_green_cap[] = {10, 11, 12, 12, 15};     // Les heures de fin des périodes sur une journée CAP
 int minutes_green_cap[] = {15, 22, 30, 22, 30};   // Les minutes de fin des périodes sur une journée CAP
 int hours_yellow_cap[] = {9, 10, 11, 13, 14};     // Les heures d'alerte au commencement des périodes sur une journée CAP
-int minutes_yellow_cap[] = {13, 20, 28, 20, 28};  // Les minutes d'alerte au commencement des périodes sur une journée CAP
+int minutes_yellow_cap[] = {13, 20, 28, 20, 28};  // Les minutes d'alerte au commencement des périodes sur une journée CAP (2 minutes avant la cloche)
 int del[] = {11, 12, 13};                         // DELs: Rouge, Jaune, Vert
-int btn = 10;                                     // Bouton pour l'alarme à feu
+int btn = 10;                                     // Bouton pour activer le mode pour l'alarme à feu
 boolean wed = false;                              // Est-ce que c'est une journée CAP (mercredi)?
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Ouvrir la communication en série entre l'ordinateur et l'Arduino
 
-  for(int i = 0; i < 3; i++) {
-    pinMode(del[i], OUTPUT);
+  for(int i = 0; i < 3; i++) {  // Pour chacune des 3 DELs,
+    pinMode(del[i], OUTPUT);    //// Définir en tant que sortie
   }
-  pinMode(btn, INPUT);
+  pinMode(btn, INPUT);          // Définir bouton d'alarme à feu en tant qu'entrée
 
-  digitalWrite(del[2], HIGH);
+  digitalWrite(del[2], HIGH); // Commencer sketch avec DEL verte allumée
 }
 
 void loop() {
-  Serial.println('7');
-  processSyncMessage();
+  Serial.println('7');  // Envoyer la requête de synchronisation au sketch Processing
+  processSyncMessage(); // VOIR FONCTION processSyncMessage
 
   time_t t = now(); // Pour éviter des ambiguités si le temps actuel change de minute durant la boucle
   
-  switch(weekday(t)) {
-    case 0:
-      Serial.println("Saturday");
-      wed = false;
+  switch(weekday(t)) {              // Si le jour de la semaine est...
+    case 0:                         //// ...samedi, alors
+      Serial.println("Saturday");   ////// Envoyer un message au sketch Processing
+      wed = false;                  ////// Ce n'est pas mercredi (journée normale)
       break;
-    case 1:
-      Serial.println("Sunday");
-      wed = false;
+    case 1:                         //// ...dimanche, alors
+      Serial.println("Sunday");     ////// Envoyer un message au sketch Processing
+      wed = false;                  ////// Ce n'est pas mercredi (journée normale)
       break;
-    case 2:
-      Serial.println("Monday");
-      wed = false;
+    case 2:                         //// ...lundi, alors
+      Serial.println("Monday");     ////// Envoyer un message au sketch Processing
+      wed = false;                  ////// Ce n'est pas mercredi (journée normale)
       break;
-    case 3:
-      Serial.println("Tuesday");
-      wed = false;
+    case 3:                         //// ...mardi, alors
+      Serial.println("Tuesday");    ////// Envoyer un message au sketch Processing
+      wed = false;                  ////// Ce n'est pas mercredi (journée normale)
       break;
-    case 4:
-      Serial.println("Wednesday");
-      wed = true;
+    case 4:                         //// ...mercredi, alors
+      Serial.println("Wednesday");  ////// Envoyer un message au sketch Processing
+      wed = true;                   ////// C'est mercredi (journée CAP)
       break;
-    case 5:
-      Serial.println("Thursday");
-      wed = false;
+    case 5:                         //// ...jeudi, alors
+      Serial.println("Thursday");   ////// Envoyer un message au sketch Processing
+      wed = false;                  ////// Ce n'est pas mercredi (journée normale)
       break;
-    case 6:
-      Serial.println("Friday");
-      wed = false;
+    case 6:                         //// ...vendredi, alors
+      Serial.println("Friday");     ////// Envoyer un message au sketch Processing
+      wed = false;                  ////// Ce n'est pas mercredi (journée normale)
       break;
   }
 
-  Serial.println(hour(t));
-  Serial.println(minute(t));
+  Serial.println(hour(t));    // Envoyer l'heure au sketch Processing
+  Serial.println(minute(t));  // Envoyer la minute au sketch Processing
 
-  if(wed == false) {
-    if(digitalRead(btn) != 1) {
-      for(int i = 0; i < 4; i++) {
-        if(((hour(t) * 60 + minute(t)) >= (hours_red_reg[i] * 60 + minutes_red_reg[i])) && ((hour(t) * 60 + minute(t)) < (hours_green_reg[i] * 60 + minutes_green_reg[i]))) {
-          Serial.println("The hours + minutes align red reg");
-          for(int j = 0; j < 3; j++) {
-            digitalWrite(del[j], LOW);
+  if(wed == false) {                                                                                                                                                          // Si c'est une journée normale, alors
+    if(digitalRead(btn) != 1) {                                                                                                                                               //// Si le bouton d'alarme à feu n'a pas été pesé, alors
+      for(int i = 0; i < 4; i++) {                                                                                                                                            //// Pour chaque élément dans la liste des débuts de période pour une journée normale,
+        if(((hour(t) * 60 + minute(t)) >= (hours_red_reg[i] * 60 + minutes_red_reg[i])) && ((hour(t) * 60 + minute(t)) < (hours_green_reg[i] * 60 + minutes_green_reg[i]))) { ////// Si le temps actuel est XYZ que le temps de commencement de la période, alors
+          Serial.println("En classe");                                                                                                                                        //////// Envoyer un message que c'est le temps des classes
+          for(int j = 0; j < 3; j++) {                                                                                                                                        ////////// Pour chacune des 3 DELs,
+            digitalWrite(del[j], LOW);                                                                                                                                        //////////// Éteindre
           }
-          digitalWrite(del[0], HIGH);
+          digitalWrite(del[0], HIGH);                                                                                                                                         ////////// Allumer DEL rouge
         }
       }
-      for(int i = 0; i < 4; i++) {
-        if(((hour(t) * 60 + minute(t)) >= (hours_green_reg[i] * 60 + minutes_green_reg[i])) && ((hour(t) * 60 + minute(t)) < (hours_yellow_reg[i] * 60 + minutes_yellow_reg[i]))) {
-          Serial.println("The hours + minutes align green reg");
+      for(int i = 0; i < 4; i++) {                                                                                                                                                  //// Pour chaque élément dans la liste des débuts de pause pour une journée normale,
+        if(((hour(t) * 60 + minute(t)) >= (hours_green_reg[i] * 60 + minutes_green_reg[i])) && ((hour(t) * 60 + minute(t)) < (hours_yellow_reg[i] * 60 + minutes_yellow_reg[i]))) { ////// Si le temps actuel est XYZ que le temps de commencement de la pause, alors
+          Serial.println("En pause");                                                                                                                                               //////// 
           for(int j = 0; j < 3; j++) {
             digitalWrite(del[j], LOW);
           }
@@ -154,23 +154,23 @@ void loop() {
     }
   }
 
-  delay(5000); // Check every 5 seconds
+  delay(5000); // Attendre 5 secondes avant de répéter la boucle
 }
 
 void processSyncMessage() {
-  // if time sync available from serial port, update time and return true
-  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of header & 10 ASCII digits
-    char c = Serial.read() ;
-    Serial.print(c);
-    if( c == TIME_HEADER ) {
-      time_t pctime = 0;
-      for(int i=0; i < TIME_MSG_LEN -1; i++){
-        c = Serial.read();
-        if( c >= '0' && c <= '9'){
-          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number
+  // Si le sketch Processing a envoyé un message de synchronisation du temps, mettre à jour l'horloge de l'Arduino
+  while(Serial.available() >=  TIME_MSG_LEN ){  // Tant que le message reçu est de la bonne longueur, alors
+    char c = Serial.read() ;                    //// Lire le message, un caractère à la fois
+    Serial.print(c);                            //// Afficher le message sur le moniteur en série, un caractère à la fois
+    if( c == TIME_HEADER ) {                    //// Si le caractère est l'en-tête du message qui démarque une synchronisation du temps, alors
+      time_t pctime = 0;                        ////// Créer nouvelle variable de temps vide
+      for(int i=0; i < TIME_MSG_LEN -1; i++){   ////// Pour chaque caractère dans le message de synchronisation,
+        c = Serial.read();                      //////// Lire le message, un caractère à la fois
+        if( c >= '0' && c <= '9'){              //////// Si le caractère est un nombre (indiquant le temps), alors
+          pctime = (10 * pctime) + (c - '0');   ////////// La variable de temps vide prend le nouveau temps envoyé par le sketch Processing
         }
       }
-      setTime(pctime);   // Sync Arduino clock to the time received on the serial port
+      setTime(pctime);                          ////// Synchroniser l'horloge interne de l'Arduino avec le nouveau temps reçu
     }
   }
 }
